3a06096cb8a781c3d0a6a5b3a2584bd4ea5c4cf0
isivroag <isivroag@gmail.com>
2021-11-30 15:56:56 -0600

    INICIO 30NOV21

---
+++ inicio.php
@@ -0,0 +1,48 @@
+<?php
+$pagina = 'home';
+include_once "templates/header.php";
+include_once "templates/barra.php";
+include_once "templates/navegacion.php";
+
+
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+
+
+  <!-- Main content -->
+  <section class="content m-0 p-0">
+    <div class="container-fluid m-0 p-0">
+      <div class="card ">
+        <div class="card-header text-white" style="background-color:#92bcdf">
+          <h3>BIENVENIDO A BOMBONELA</h3>
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="container-fluid m-0 p-0">
+     
+      <div class="jumbotron bg-white">
+        <div class="row justify-content-center">
+          <div class="col-lg-12 text-center">
+            <h1 class="titulo-fisioost text-bold"></h1>
+            <br>
+            <img src="img/logocompleto.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /.content -->
+</div>
+
+
+<?php
+include_once 'templates/footer.php';
+?>
+<script src="fjs/cards.js"></script>
